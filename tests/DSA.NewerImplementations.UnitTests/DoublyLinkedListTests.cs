1e8b61fe595599a8dc86fe297128e68dbf2bb540
MaximCote <maximcotedeveloper@duck.com>
2023-08-26 15:46:31 -0400

    test: Adding test constants. Renaming methods to indicate the type of of the contained value they test. Adding tests for public methods.

--- tests/DSA.NewerImplementations.UnitTests/DoublyLinkedListTests.cs
+++ tests/DSA.NewerImplementations.UnitTests/DoublyLinkedListTests.cs
@@ -1,18 +1,91 @@
 ﻿using FluentAssertions;
 using DSA.NewerImplementations;
+using System.Collections.Generic;
 
 namespace DSA.NewerImplementations.UnitTests;
 
 public class DoublyLinkedListTests
 {
+    #region Seed constants
+    
+    #region Integer constants
+    public const Int32 firstIntegerSeed = 1;
+    public const Int32 secondIntegerSeed = 2;
+    public const Int32 thirdIntegerSeed = 3;
+    public const Int32 fourthIntegerSeed = 7;
+    public const Int32 fifthIntegerSeed = 8;
+    public const Int32 sixthIntegerSeed = 9;
+    #endregion Integer constants
+
+    #region String constants
+    public const String firstStringSeed = "one";
+    public const String secondStringSeed = "two";
+    public const String thirdStringSeed = "three";
+    public const String fourthStringSeed = "seven";
+    public const String fifthStringSeed = "eight";
+    public const String sixthStringSeed = "nine";
+    #endregion String constants
+
+    #endregion Seed constants
+
+    private DoublyLinkedList<Int32> _SeededIntList = new ();
+    private DoublyLinkedList<String> _SeededStringList = new ();
+
+    private String _SeededPrintAllExpectedValues = String.Empty;
+
+    public DoublyLinkedListTests()
+    {
+        //Seed an object with the contained type of Int32
+        //Seed by inserting at Head
+        _SeededIntList.TryInsertAtHead(firstIntegerSeed);
+        _SeededIntList.TryInsertAtHead(secondIntegerSeed);
+        _SeededIntList.TryInsertAtHead(thirdIntegerSeed);
+        //Seed by inserting at Tail
+        _SeededIntList.TryInsertAtTail(fourthIntegerSeed);
+        _SeededIntList.TryInsertAtTail(fifthIntegerSeed);
+        _SeededIntList.TryInsertAtTail(sixthIntegerSeed);
+        //
+        _SeededPrintAllExpectedValues = 
+            $"{thirdIntegerSeed} -> {secondIntegerSeed} -> {firstIntegerSeed}"
+            + $" -> " 
+            + $"{fourthIntegerSeed} -> {fifthIntegerSeed} -> {sixthIntegerSeed}";
+
+        //Seed an object with the contained type of String
+        //Seed by inserting at Head
+        _SeededStringList.TryInsertAtHead(firstStringSeed);
+        _SeededStringList.TryInsertAtHead(secondStringSeed);
+        _SeededStringList.TryInsertAtHead(thirdStringSeed);
+
+        //Seed by inserting at Tail
+        _SeededStringList.TryInsertAtTail(fourthStringSeed);
+        _SeededStringList.TryInsertAtTail(fifthStringSeed);
+        _SeededStringList.TryInsertAtTail(sixthStringSeed);
+    }
+
     #region Doubly Linked List of type Int32
 
     #region New empty list
 
     #region Static methods
+    [Fact]
+    public void StaticDuplicate_ContainedTypeInt32_GivenNewList_ShouldReturnAnotherNewList()
+    {
+        //Given
+        DoublyLinkedList<int> newLinkedList = new DoublyLinkedList<int>();
+
+        //When
+        DoublyLinkedList<int> resultingList = DoublyLinkedList<int>.Duplicate(newLinkedList);
+        String resultingListPrintOutput = resultingList.PrintAll();
+
+        //Then
+        resultingList.Should().NotBeNull();
+        resultingList.Should().NotBe(newLinkedList);
+        resultingListPrintOutput.Should().NotBeNullOrEmpty();
+        resultingListPrintOutput.Should().Be(DoublyLinkedList<int>.EmptyListReport);
+    }
 
     [Fact]
-    public void Intersect_GivenArgumentsTwoNewLists_ShouldReturnEmptyList()
+    public void Intersect_ContainedTypeInt32_GivenArgumentsTwoNewLists_ShouldReturnEmptyList()
     {
         //Given
         DoublyLinkedList<int> newLinkedList = new DoublyLinkedList<int>();
@@ -27,7 +100,7 @@ public class DoublyLinkedListTests
     }
 
     [Fact]
-    public void Unify_GivenNewListAndArgumentIsNewList_ShouldReturnEmptyList()
+    public void Unify_ContainedTypeInt32_GivenNewListAndArgumentIsNewList_ShouldReturnEmptyList()
     {
         //Given
         DoublyLinkedList<int> newLinkedList = new DoublyLinkedList<int>();
@@ -44,7 +117,7 @@ public class DoublyLinkedListTests
     #endregion Static methods
 
     [Fact]
-    public void New_GivenTypeInt32_ShouldReturnTypeDoublyLinkedListWithTypeInt32()
+    public void New_ContainedTypeInt32_GivenTypeInt32_ShouldReturnTypeDoublyLinkedListWithTypeInt32()
     {
         //Given
         DoublyLinkedList<int> newLinkedList = new DoublyLinkedList<int>();
@@ -54,7 +127,7 @@ public class DoublyLinkedListTests
     }
 
     [Fact]
-    public void IsEmpty_GivenNewList_ShouldReturnTrue()
+    public void IsEmpty_ContainedTypeInt32_GivenNewList_ShouldReturnTrue()
     {
         //Given
         DoublyLinkedList<int> newLinkedList = new DoublyLinkedList<int>();
@@ -67,7 +140,7 @@ public class DoublyLinkedListTests
     }
 
     [Fact]
-    public void HasDetectedLoop_GivenNewList_ShouldReturnFalse()
+    public void HasDetectedLoop_ContainedTypeInt32_GivenNewList_ShouldReturnFalse()
     {
         //Given
         DoublyLinkedList<int> newLinkedList = new DoublyLinkedList<int>();
@@ -80,7 +153,7 @@ public class DoublyLinkedListTests
     }
 
     [Fact]
-    public void PrintAll_GivenNewList_ShouldReturnStringWithEmptyListMessage()
+    public void PrintAll_ContainedTypeInt32_GivenNewList_ShouldReturnStringWithEmptyListMessage()
     {
         //Given
         DoublyLinkedList<int> newLinkedList = new DoublyLinkedList<int>();
@@ -94,7 +167,7 @@ public class DoublyLinkedListTests
     }
 
     [Fact]
-    public void PrintUniqueValues_GivenNewList_ShouldReturnStringWithEmptyListMessage()
+    public void PrintUniqueValues_ContainedTypeInt32_GivenNewList_ShouldReturnStringWithEmptyListMessage()
     {
         //Given
         DoublyLinkedList<int> newLinkedList = new DoublyLinkedList<int>();
@@ -108,7 +181,7 @@ public class DoublyLinkedListTests
     }
 
     [Fact]
-    public void GetLength_GivenNewList_ShouldReturnZero()
+    public void GetLength_ContainedTypeInt32_GivenNewList_ShouldReturnZero()
     {
         //Given
         DoublyLinkedList<int> newLinkedList = new DoublyLinkedList<int>();
@@ -122,7 +195,7 @@ public class DoublyLinkedListTests
     }
 
     [Fact]
-    public void TryRemoveAtHead_GivenNewList_ShouldReturnFalse()
+    public void TryRemoveAtHead_ContainedTypeInt32_GivenNewList_ShouldReturnFalse()
     {
         //Given
         DoublyLinkedList<int> newLinkedList = new DoublyLinkedList<int>();
@@ -135,7 +208,7 @@ public class DoublyLinkedListTests
     }
 
     [Fact]
-    public void TryRemoveAtTail_GivenNewList_ShouldReturnFalse()
+    public void TryRemoveAtTail_ContainedTypeInt32_GivenNewList_ShouldReturnFalse()
     {
         //Given
         DoublyLinkedList<int> newLinkedList = new DoublyLinkedList<int>();
@@ -148,7 +221,7 @@ public class DoublyLinkedListTests
     }
 
     [Fact]
-    public void TryRemoveFromHead_GivenNewListAndArgumentZero_ShouldReturnFalse()
+    public void TryRemoveFromHead_ContainedTypeInt32_GivenNewListAndArgumentZero_ShouldReturnFalse()
     {
         //Given
         DoublyLinkedList<int> newLinkedList = new DoublyLinkedList<int>();
@@ -162,7 +235,7 @@ public class DoublyLinkedListTests
     }
 
     [Fact]
-    public void TryRemoveFromHead_GivenNewListAndArgumentOtherThanZero_ShouldReturnFalse()
+    public void TryRemoveFromHead_ContainedTypeInt32_GivenNewListAndArgumentOtherThanZero_ShouldReturnFalse()
     {
         //Given
         DoublyLinkedList<int> newLinkedList = new DoublyLinkedList<int>();
@@ -176,7 +249,7 @@ public class DoublyLinkedListTests
     }
 
     [Fact]
-    public void TryRemoveFromTail_GivenNewListAndArgumentZero_ShouldReturnFalse()
+    public void TryRemoveFromTail_ContainedTypeInt32_GivenNewListAndArgumentZero_ShouldReturnFalse()
     {
         //Given
         DoublyLinkedList<int> newLinkedList = new DoublyLinkedList<int>();
@@ -190,7 +263,7 @@ public class DoublyLinkedListTests
     }
 
     [Fact]
-    public void TryRemoveFromTail_GivenNewListAndArgumentOtherThanZero_ShouldReturnFalse()
+    public void TryRemoveFromTail_ContainedTypeInt32_GivenNewListAndArgumentOtherThanZero_ShouldReturnFalse()
     {
         //Given
         DoublyLinkedList<int> newLinkedList = new DoublyLinkedList<int>();
@@ -204,7 +277,7 @@ public class DoublyLinkedListTests
     }
 
     [Fact]
-    public void TryReverse_GivenNewList_ShouldReturnFalse()
+    public void TryReverse_ContainedTypeInt32_GivenNewList_ShouldReturnFalse()
     {
         //Given
         DoublyLinkedList<int> newLinkedList = new DoublyLinkedList<int>();
@@ -217,7 +290,7 @@ public class DoublyLinkedListTests
     }
 
     [Fact]
-    public void TryRemoveDuplicateValues_GivenNewList_ShouldReturnFalse()
+    public void TryRemoveDuplicateValues_ContainedTypeInt32_GivenNewList_ShouldReturnFalse()
     {
         //Given
         DoublyLinkedList<int> newLinkedList = new DoublyLinkedList<int>();
@@ -230,7 +303,7 @@ public class DoublyLinkedListTests
     }
 
     [Fact]
-    public void IntersectWith_GivenNewListAndArgumentIsNewList_ShouldReturnEmptyList()
+    public void IntersectWith_ContainedTypeInt32_GivenNewListAndArgumentIsNewList_ShouldReturnEmptyList()
     {
         //Given
         DoublyLinkedList<int> newLinkedList = new DoublyLinkedList<int>();
@@ -245,7 +318,7 @@ public class DoublyLinkedListTests
     }
 
     [Fact]
-    public void UnifyWith_GivenNewListAndArgumentIsNewList_ShouldReturnEmptyList()
+    public void UnifyWith_ContainedTypeInt32_GivenNewListAndArgumentIsNewList_ShouldReturnEmptyList()
     {
         //Given
         DoublyLinkedList<int> newLinkedList = new DoublyLinkedList<int>();
@@ -258,11 +331,135 @@ public class DoublyLinkedListTests
         //Then
         flag.Should().BeTrue();
     }
-
     #endregion New empty list
 
+    #region Seeded list
 
+    #region Static methods
+    [Fact]
+    public void StaticDuplicate_ContainedTypeInt32_GivenArgumentListWithValues_ShouldReturnNewListWithEqualValues()
+    {
+        //Given
+        DoublyLinkedList<int> seededObject = _SeededIntList;
+        String listToDuplicatePrintOutput = seededObject.PrintAll();
 
+        //When
+        DoublyLinkedList<int> resultingList = DoublyLinkedList<int>.Duplicate(seededObject);
+        String resultingListPrintOutput = resultingList.PrintAll();
+
+        //Then
+        resultingList.Should().NotBeNull();
+        resultingListPrintOutput.Should().BeEquivalentTo(listToDuplicatePrintOutput);
+    }
+
+    [Fact]
+    public void StaticIntersect_ContainedTypeInt32_GivenArgumentsTwoListsWithNoValueOverlap_ShouldReturnEmptyList()
+    {
+        //Given
+        DoublyLinkedList<int> duplicatedSeededObject = DoublyLinkedList<int>.Duplicate(_SeededIntList);
+        DoublyLinkedList<int> otherList = new DoublyLinkedList<int>();
+        otherList.TryInsertAtHead(6);
+        otherList.TryInsertAtHead(5);
+        otherList.TryInsertAtHead(4);
+
+        //When
+        DoublyLinkedList<int> resultingList = DoublyLinkedList<int>.Intersect(duplicatedSeededObject, otherList);
+        Boolean flag = resultingList.IsEmpty();
+
+        //Then
+        flag.Should().BeTrue();
+    }
+
+    [Fact]
+    public void StaticUnify_ContainedTypeInt32_GivenSeededListAndArgumentIsListWithNoValueOverlap_ShouldReturnConcatenatedList()
+    {
+        //Given
+        DoublyLinkedList<int> duplicatedSeededObject = DoublyLinkedList<int>.Duplicate(_SeededIntList);
+        DoublyLinkedList<int> otherList = new DoublyLinkedList<int>();
+        otherList.TryInsertAtHead(6);
+        otherList.TryInsertAtHead(5);
+        otherList.TryInsertAtHead(4);
+        String duplicatedSeededObjectPrintOutput = duplicatedSeededObject.PrintAll();
+        String otherListPrintOutput = otherList.PrintAll();
+
+        //When
+        DoublyLinkedList<int> resultingList = DoublyLinkedList<int>.Unify(duplicatedSeededObject, otherList);
+        String resultingListPrintOutput = resultingList.PrintAll();
+
+        //Then
+        resultingListPrintOutput.Should().Be(duplicatedSeededObjectPrintOutput + $" -> " + otherListPrintOutput);
+    }
+    #endregion Static methods
+
+    [Fact]
+    public void IsEmpty_ContainedTypeInt32_GivenSeededList_ShouldReturnFalse()
+    {
+        //Given
+        DoublyLinkedList<int> seededObject = _SeededIntList;
+
+        //When
+        Boolean flag = seededObject.IsEmpty();
+
+        //Then
+        flag.Should().BeFalse();
+    }
+
+    [Fact]
+    public void HasDetectedLoop_ContainedTypeInt32_GivenSeededListWithNoLoop_ShouldReturnFalse()
+    {
+        //Given
+        DoublyLinkedList<int> seededObject = _SeededIntList;
+
+        //When
+        Boolean flag = seededObject.HasDetectedLoop();
+
+        //Then
+        flag.Should().BeFalse();
+    }
+
+    [Fact]
+    public void PrintAll_ContainedTypeInt32_GivenSeededList_ShouldReturnStringWithAllSeededValues()
+    {
+        //Given
+        DoublyLinkedList<int> seededObject = _SeededIntList;
+        String expectedValue = _SeededPrintAllExpectedValues;
+
+        //When
+        String resultOutput = seededObject.PrintAll();
+
+        //Then
+        resultOutput.Should().Be(expectedValue);
+    }
+
+    [Fact]
+    public void PrintUniqueValues_ContainedTypeInt32_GivenSeededList_ShouldReturnStringWithAllSeededUniqueValues()
+    {
+        //Given
+        DoublyLinkedList<int> seededObject = _SeededIntList;
+        String expectedValue = _SeededPrintAllExpectedValues;
+
+        //When
+        String resultOutput = seededObject.PrintUniqueValues();
+
+        //Then
+        resultOutput.Should().Be(expectedValue);
+    }
+
+    [Fact]
+    public void GetLength_ContainedTypeInt32_GivenSeededList_ShouldNotReturnZero()
+    {
+        //Given
+        DoublyLinkedList<int> seededObject = _SeededIntList;
+        UInt32 unexpected = 0;
+
+        //When
+        UInt32 report = seededObject.GetLength();
+
+        //Then
+        report.Should().NotBe(unexpected);
+    }
+
+    #endregion Seeded list
 
     #endregion Doubly Linked List of type Int32
 }
